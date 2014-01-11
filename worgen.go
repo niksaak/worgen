ab9d7787a2057b928d3eb670deefe19cd06356f5
Mikola Samardak <niksaak@gmail.com>
2014-01-12 01:12:53 +0200

    write documentation

--- worgen.go
+++ worgen.go
@@ -1,3 +1,4 @@
+// Package worgen implements generating pronounceable words from integers.
 package worgen
 
 import (
@@ -26,8 +27,9 @@ func genran(ran *rand.Rand) (s string) {
 	return s
 }
 
-func Wor(id int64, count int) (s string) {
-	ran := rand.New(rand.NewSource(int64(id)))
+// Wor generates string of count words for 64 bit integer n.
+func Wor(n int64, count int) (s string) {
+	ran := rand.New(rand.NewSource(int64(n)))
 	for i := 0; i < count; i++ {
 		s += genran(ran)
 		s += genran(ran)
@@ -36,18 +38,24 @@ func Wor(id int64, count int) (s string) {
 	return s[:len(s)-1]
 }
 
+// Wor16 generates string of one word for 16 bit integer n.
 func Wor16(n int16) string {
 	return Wor(int64(n), 1)
 }
 
+// Wor32 generates string of two words for 32 bit integer n.
 func Wor32(n int32) string {
 	return Wor(int64(n), 2)
 }
 
+// Wor64 generates string of four words for 32 bit integer n.
 func Wor64(n int64) string {
 	return Wor(int64(n), 4)
 }
 
+// WorBig generates string for multi-precision integer n, count of words
+// calculated as:
+//     (bitLength(n) / 16) rounded towards infinity
 func WorBig(n *big.Int) string {
 	if n.BitLen() < 64 {
 		return Wor64(n.Int64())
